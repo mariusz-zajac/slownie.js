dc892579c0c01bfb8a4fcf6706fe509366cbe67f
Michał Wołoszyn <fraunos@gmail.com>
2018-11-04 05:23:38 +0100

    refactor

--- README.md
+++ README.md
@@ -1,35 +1,23 @@
 Slownie.js
 ==========
 
-Little library for converting numbers into polish words. 
-Ported from C version at found at http://4programmers.net/Pastebin/1668
+Little library for converting numbers into polish words.
+
+Fork of https://github.com/exu/slownie.js which was ported from C version found at http://4programmers.net/Pastebin/1668
 
 Installation
 ------------
 
 ```
-$ npm install slownie
-```
-or 
-
-```
-{ "name" : "my-project"
-, "version" : "1.0.0"
-, "dependencies" : { "slownie" : ">= 1.0.0" } }
-```
-
-and then 
-
-```
-$ npm install
+$ npm install slownie --save
 ```
 
 Usage example
 -------------
 ```
 
-var Slownie = new require('slownie').Slownie();
-console.log( Slownie.get(1939393822) );
+const slownie = require('slownie')
+console.log( slownie(1939393822) );
 
 ```
 
--- test/test.js
+++ test/test.js
@@ -1,26 +1,26 @@
 require('should')
-var Slownie = require('../lib/slownie').Slownie
+const slownie = require('../lib/slownie')
 
-describe('Slownie', function(){
+describe('slownie', function(){
 
   it("should be able to transform short into corresponding words", function(){
-    new Slownie().get(123).should.equal('sto dwadzieścia trzy');
+    slownie(123).should.equal('sto dwadzieścia trzy');
   });
 
   it("should be able to transform long numbers into corresponding words", function(){
-    new Slownie().get(1000000).should.equal('milion');
+    slownie(1000000).should.equal('milion');
   });
 
   it("should be able to transform long numbers into corresponding words", function(){
-    new Slownie().get(1000).should.equal('tysiąc');
+    slownie(1000).should.equal('tysiąc');
   });
 
   it("should be able to transform medium length numbers into corresponding words", function(){
-    new Slownie().get(29837373765).should.equal('dwadzieścia dziewięć miliardów osiemset trzydzieści siedem milionów trzysta siedemdziesiąt trzy tysiące siedemset sześćdziesiąt pięć');
+    slownie(29837373765).should.equal('dwadzieścia dziewięć miliardów osiemset trzydzieści siedem milionów trzysta siedemdziesiąt trzy tysiące siedemset sześćdziesiąt pięć');
   });
 
   it("should be able to transform freaking numbers into corresponding words", function(){
-    new Slownie().get(100200300400).should.equal('sto miliardów dwieście milionów trzysta tysięcy czterysta');
+    slownie(100200300400).should.equal('sto miliardów dwieście milionów trzysta tysięcy czterysta');
   });
 
 });
